29c3efb38b27d6ee19125492728d448e87427e8d
rodrigonassif <rodrigonassif@outlook.com>
2025-05-22 01:03:46 +0000

    criação do nível aventureiro para o desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,10 @@ int main() {
     int populacao1, populacao2; // criar as variáveis populacao1 e populacao2 do tipo inteiro
     float area1, area2; // criar as variáveis area1 e area2 do tipo float
     float pib1, pib2; // criar as variáveis pib1 e pib2 do tipo float
-    int pontos_turisticos1, pontos_turisticos2; // criar as variáveis ponto_turisticos1 e ponto_turisticos2 do tipo inteiro 
-    
+    int pontos_turisticos1, pontos_turisticos2; // criar as variáveis ponto_turisticos1 e ponto_turisticos2 do tipo inteiro
+    float densidadepopulacional1, densidadepopulacional2; // criar as variáveis densidadepopulacional e densidadepopulacional2 do tipo float 
+    float pibpercapita1, pibpercapita2; // criar as variáveis pibpercapita1 e pibpercapita2 do tipo float
+
     printf("Cadastro da Carta 1:\n"); // exibir na tela o titulo Cadastro de Cartas
     printf("Estado (A-H): "); // exibir na tela a solicitação de entrada de dados para a variável estado1
     scanf(" %c", &estado1); // exibir o prompt de entrada de dados para a variável estado1
@@ -24,6 +26,8 @@ int main() {
     scanf(" %f", &pib1); // exibir o prompt de entrada de dados para a variável pib1
     printf("Número de Pontos Turísticos: "); // exibir na tela a solicitação de entrada de dados para a variável ponto_turisticos1
     scanf(" %d", &pontos_turisticos1); // exibir o prompot de entrada de dados para a variável ponto_turisticos1
+    densidadepopulacional1 = (float)(populacao1 / area1); // calcula o valor da variavel densidadepopulacional1 para a Carta 1
+    pibpercapita1 = (pib1 * 1000000000.0f) / populacao1; // converte os valores do resultado da variável pibpercapita1
 
     /*
     o próximo trecho de código , foi codificado nas mesmas diretrizes do código anterior, para as segundas variáveis de mesmo tipo
@@ -47,6 +51,9 @@ int main() {
     scanf(" %f", &pib2);
     printf("Número de Pontos Turísticos: ");
     scanf(" %d", &pontos_turisticos2);
+    densidadepopulacional2 = (float)(populacao2 / area2); // calcula o valor da variavel densidadepopulacional1 para a Carta 2
+    pibpercapita2 = (pib2 * 1000000000.0f) / populacao2; // converte os valores do resultado da variável pibpercapita2
+
     /*
     o próximo trecho de codificação em C, demonstra todas as informações das Cartas 1 e 2, apresentadas na tela através da função printf
     e processadas corretamente conforme todas as formatações anteriores , respeitando o tipo das variáveis anteriormente declaradas
@@ -61,6 +68,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional1);
+    printf("O PIB per Capita é: %.2f reais\n", pibpercapita1);
     
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -70,6 +79,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional2);
+    printf("O PIB per Capita é: %.2f reais\n", pibpercapita2);
     
     return 0;
 }
